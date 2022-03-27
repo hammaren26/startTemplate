8ea45fef4ed907a85eaee806bd70f24df3e243b3
Vyacheslav Tulaev <hammaren23@gmail.com>
2022-03-27 16:04:45 +0600

    add popup

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,18 +2,14 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
-
-
-// window.$ = jQuery;
-// window.jQuery = jQuery;
-// import jQuery from 'jquery';
-
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
-import 'selectric'
+import 'selectric';
 
 
 $(function () {
-   $('.custom-select').selectric({});
+   $('.custom-select').selectric({
+
+   });
 });
 
--- src/js/files/tippy.js
+++ src/js/files/tippy.js
@@ -2,13 +2,26 @@
 import tippy from 'tippy.js';
 
 // Подключение cтилей из src/scss/libs
-import "../../scss/libs/tippy.scss";
+// import "../../scss/libs/tippy.scss";
 // Подключение cтилей из node_modules
-//import 'tippy.js/dist/tippy.css';
+// import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
+// import 'tippy.js/themes/light.css';
+
+const template = document.getElementById('example')
+
+console.log(template);
 
 export function initTippy() {
-	tippy('[data-tippy-content]', {
-
-	});
+   tippy('[data-tippy-content]', {
+      // content: '<div class="test">v rot ebal</div>',
+      arrow: true,
+      content: template,
+      allowHTML: true,
+      arrow: false,
+      trigger: 'click',
+      interactive: true,
+      animation: 'scale'
+   });
 }
 initTippy();
