2110267452d4bb3bd94033aaf206552d3a29c59e
Vyacheslav Tulaev <hammaren23@gmail.com>
2022-04-28 21:23:42 +0600

    ddd

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,9 +3,22 @@
 // import { formsModules } from "./forms/forms.js";
 
 import jQuery from "jquery";
+import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
 window.$ = window.jQuery = jQuery;
 import 'selectric';
 import { checkInput, closest } from "./functions.js";
+import lightGallery from 'lightgallery';
+import 'lightgallery/css/lightgallery-bundle.min.css';
 
 
+lightGallery(document.getElementById('lightgallery'), {
+   speed: 500,
+   controls: true
+});
 
+
+const player = document.querySelector('.player');
+const playBtn = document.querySelector('.play');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
